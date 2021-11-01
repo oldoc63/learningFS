a8e70cbb4533978965d388405ce131037f6218f1
oldoc63 <leopoldo.olmos@gmail.com>
2021-11-01 14:53:06 -0400

    += reassign a new value to the variable #7

--- introToJS/main.js
+++ introToJS/main.js
@@ -1,4 +1,16 @@
-const entree = 'Enchiladas';
-console.log(entree);
-//entree = 'Tacos';
-//const testing;
+let levelUp = 10;
+levelUp += 5;
+let powerLevel = 9001;
+powerLevel -= 100;
+let multiplyMe = 32;
+multiplyMe *= 11;
+let quarterMe = 1152;
+quarterMe /= 4;
+
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements. 
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
