ac656fa3c69df5dabb8a182361e8c795509e8973
oldoc63 <leopoldo.olmos@gmail.com>
2021-11-04 12:24:20 -0400

    Ask about your future #23

--- magic8Ball/main.js
+++ magic8Ball/main.js
@@ -1,2 +1,4 @@
-userName = '';
+userName = 'Leopoldo';
 userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+const userQuestion = `${userName}: What can you tell about my future?`
+console.log(userQuestion)
