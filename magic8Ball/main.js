88608ad6ae279b1f63707e2b50391decbaca41fc
oldoc63 <leopoldo.olmos@gmail.com>
2021-11-04 14:20:02 -0400

    Generate a random number between 0 and 7 #25

--- magic8Ball/main.js
+++ magic8Ball/main.js
@@ -2,3 +2,5 @@ userName = 'Leopoldo';
 userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
 const userQuestion = `${userName}: What can you tell about my future?`
 console.log(userQuestion)
+const randomNumber = Math.floor(Math.random() * 8);
+
