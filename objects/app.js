e2998b2cbee6186c0bfdf0567341b3ed23257162
oldoc63 <leopoldo.olmos@gmail.com>
2021-11-24 19:13:06 -0400

    We store that functionality in methods on our objects #80

--- objects/app.js
+++ objects/app.js
@@ -1,34 +1,7 @@
-let spaceship = {
-    crew: {
-    captain: { 
-        name: 'Lily', 
-        degree: 'Computer Engineering', 
-        cheerTeam() { console.log('You got this!') } 
-        },
-    'chief officer': { 
-        name: 'Dan', 
-        degree: 'Aerospace Engineering', 
-        agree() { console.log('I agree, captain!') } 
-        },
-    medic: { 
-        name: 'Clementine', 
-        degree: 'Physics', 
-        announce() { console.log(`Jets on!`) } },
-    translator: {
-        name: 'Shauna', 
-        degree: 'Conservation Science', 
-        powerFuel() { console.log('The tank is full!') } 
-        }
-    }
-}; 
-
-// Write your code below
-for (let crew_i in spaceship.crew){
-  console.log(`${crew_i}: ${spaceship.crew[crew_i].name}`);
-}
-
-for (let crew_i in spaceship.crew){
-  console.log(`${spaceship.crew[crew_i].name} : ${spaceship.crew[crew_i].degree}`);
-}
-
+const goat = {
+	dietType: 'herbivore',
+	makeSound () {
+		console.log('baaa');
+	}
+};
 
