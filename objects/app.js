97b0d241097adc75ec70700ed668dfa957733fe5
oldoc63 <leopoldo.olmos@gmail.com>
2021-11-24 16:00:15 -0400

    functions can mutate the object permanently #75

--- objects/app.js
+++ objects/app.js
@@ -1,33 +1,17 @@
 let spaceship = {
-  passengers: [{name: 'Space Dog'}, {name: 'Space Bird'}, {name: 'Space Cat'}],
-  telescope: {
-    yearBuilt: 2018,
-    model: "91031-XLT",
-    focalLength: 2032 
-  },
-  crew: {
-    captain: { 
-      name: 'Sandra', 
-      degree: 'Computer Engineering', 
-      encourageTeam() { console.log('We got this!') },
-      'favorite foods': ['cookies', 'cakes', 'candy', 'spinach']
-       }
-  },
-  engine: {
-    model: "Nimbus2000"
-  },
-  nanoelectronics: {
-    computer: {
-      terabytes: 100,
-      monitors: "HD"
-    },
-    'back-up': {
-      battery: "Lithium",
-      terabytes: 50
-    }
-  }
-}; 
+	'Fuel Type': 'Turbo Fuel',
+	homePlanet: 'Earth'
+};
 
-const capFave = spaceship.crew.captain['favorite foods'][0];
+let greenEnergy = obj => {
+	obj['Fuel Type'] = 'avocado oil';
+}
 
-const firstPassenger = spaceship.passengers[0];
+let remotelyDisable = obj => {
+	obj.disabled = true;
+}
+
+greenEnergy(spaceship);
+remotelyDisable(spaceship);
+
+console.log(spaceship);
