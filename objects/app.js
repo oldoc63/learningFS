c690d1ebd5c3d3e285fb7086503962b6d9c2e810
oldoc63 <leopoldo.olmos@gmail.com>
2021-11-24 16:17:47 -0400

    Our loop will go through each element of the spaceship.crew object #77

--- objects/app.js
+++ objects/app.js
@@ -1,17 +1,28 @@
 let spaceship = {
-	'Fuel Type': 'Turbo Fuel',
-	homePlanet: 'Earth'
-};
-
-let greenEnergy = obj => {
-	obj['Fuel Type'] = 'avocado oil';
+  crew: {
+    captain: { 
+      name: 'Lily', 
+      degree: 'Computer Engineering', 
+      cheerTeam() { console.log('You got this!') } 
+    },
+    'chief officer': { 
+      name: 'Dan', 
+      degree: 'Aerospace Engineering', 
+      agree() { console.log('I agree, captain!') } 
+    },
+    medic: { 
+      name: 'Clementine', 
+      degree: 'Physics', 
+      announce() { console.log(`Jets on!`) } },
+    translator: {
+      name: 'Shauna', 
+      degree: 'Conservation Science', 
+      powerFuel() { console.log('The tank is full!') } 
+    }
+  }
+}; 
+ 
+// for...in
+for (let crewMember in spaceship.crew) {
+  console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`);
 }
-
-let remotelyDisable = obj => {
-	obj.disabled = true;
-}
-
-greenEnergy(spaceship);
-remotelyDisable(spaceship);
-
-console.log(spaceship);
