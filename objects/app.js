d28fe6d0f35f83501f140af86aa252c1c3679523
oldoc63 <leopoldo.olmos@gmail.com>
2021-11-27 08:02:44 -0400

    Object result of calling function() with the needed arguments #96

--- objects/app.js
+++ objects/app.js
@@ -1,25 +1,13 @@
-const robot = {
-  _model: '1E78V2',
-  _energyLevel: 100,
-  _numOfSensors: 15,
-  get numOfSensors(){
-    if(typeof this._numOfSensors === 'number'){
-      return this._numOfSensors;
-    } else {
-      return 'Sensors are currently down.'
-    }
-  },
-  set numOfSensors(num){
-    if(typeof num === 'number' && num >= 0){
-      this._numOfSensors = num;
-    } else {
-      console.log('Pass in a number that is greater than or equal to 0');
-    }
-  }
+const monsterFactory = (name, age, energySource, catchPhrase) => {
+	return {
+		name: name,
+		age: age,
+		energySource: energySource,
+		scare () {
+			console.log(catchPhrase);
+		}
+	}
+
 };
 
-robot.numOfSensors = 100;
-console.log(robot.numOfSensors);
-
-
-
+const ghost = monsterFactory('Ghouli', 251, 'ectoplasm', 'BOO!');
