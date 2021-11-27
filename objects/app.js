c5e839289be22b474d11bd549d351bca161dfab4
oldoc63 <leopoldo.olmos@gmail.com>
2021-11-26 20:29:12 -0400

    Setter example #93

--- objects/app.js
+++ objects/app.js
@@ -1,16 +1,25 @@
-const person = {
-	_age: 37,
-	set age (newAge) {
-		if (typeof newAge === 'number') {
-			this._age = newAge;
-		} else {
-			console.log('you must assign a number to age');
-		}
-	}
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num){
+    if(typeof num === 'number' && num >= 0){
+      this._numOfSensors = num;
+    } else {
+      console.log('Pass in a number that is greater than or equal to 0');
+    }
+  }
 };
 
-person.age = 40;
-console.log(person._age); // Logs: 40
-person.age = '40'; // Logs: You must assign a number to age
-person._age = 'forty-five'
-console.log(person._age); // Prints forty-five
+robot.numOfSensors = 100;
+console.log(robot.numOfSensors);
+
+
+
