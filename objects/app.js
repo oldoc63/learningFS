3d85ac635000e1dd620852280dc1694a7d270338
oldoc63 <leopoldo.olmos@gmail.com>
2021-11-27 08:24:41 -0400

    Robot Factory #96

--- objects/app.js
+++ objects/app.js
@@ -1,13 +1,13 @@
-const monsterFactory = (name, age, energySource, catchPhrase) => {
-	return {
-		name: name,
-		age: age,
-		energySource: energySource,
-		scare () {
-			console.log(catchPhrase);
-		}
-	}
-
+const robotFactory = (model, mobile) => {
+  return {
+    model: model,
+    mobile: mobile,
+    beep () {
+      console.log('Beep Boop');
+    }
+  }
 };
 
-const ghost = monsterFactory('Ghouli', 251, 'ectoplasm', 'BOO!');
+const tinCan = robotFactory('P-500', true);
+
+tinCan.beep();
