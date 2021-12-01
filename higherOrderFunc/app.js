015d6d9dd97a5d84315979de5b9ae2440e891370
oldoc63 <leopoldo.olmos@gmail.com>
2021-12-01 11:51:24 -0400

    Higher order function example #121

--- higherOrderFunc/app.js
+++ higherOrderFunc/app.js
@@ -1,17 +1,35 @@
-const timeFuncRuntime = funcParameter => {
-	let t1 = Date.now();
-	funcParameter();
-	let t2 = Date.now();
-	return t2 - t1;
-}
-
-const addOneToOne = () => 1 + 1;
-
-console.log(timeFuncRuntime(addOneToOne));
-
-console.log(timeFuncRuntime(() => {
-  for (let i = 10; i>0; i--){
-    console.log(i);
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for(let i = 1; i <= 1000000; i++) {
+    if ( (2 + 2) != 4) {
+      console.log('Something has gone very wrong :( ');
+    }
   }
-}));
+};
+
+const addTwo = num => num + 2;
+
+const timeFuncRuntime = funcParameter => {
+  let t1 = Date.now();
+  funcParameter();
+  let t2 = Date.now();
+  return t2 - t1;
+};
+
+// Write your code below
+
+const time2p2 = timeFuncRuntime(checkThatTwoPlusTwoEqualsFourAMillionTimes);
+
+console.log(time2p2);
+
+const checkConsistentOutput = (func, value) => {
+  let firstTime = func(value);
+  let secondTime = func(value);
+  if (firstTime === secondTime) {
+    return firstTime;
+  } else {
+    return 'This function returned inconsistent results';
+  }
+};
+
+checkConsistentOutput(addTwo, 3);
 
