7cc8f60b9765e337016115792b7d43f976b64bed
oldoc63 <leopoldo.olmos@gmail.com>
2021-12-02 19:40:08 -0400

    forEach used with function declaration, expression or arrow #127

--- iterators/main.js
+++ iterators/main.js
@@ -1,23 +1,5 @@
-const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 
-artists.forEach(artist => {
-  console.log(artist + ' is one of my favorite artists.');
-});
-
-const numbers = [1, 2, 3, 4, 5];
-
-const squareNumbers = numbers.map(number => {
-  return number * number;
-});
-
-console.log(squareNumbers);
-
-const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
-
-const onlyNumbers = things.filter(thing => {
-  return typeof thing === 'number';
-});
-
-console.log(onlyNumbers);
-
+// Iterate over fruits below
+fruits.forEach(fruit => console.log('I want to eat a ' + fruit));
 
