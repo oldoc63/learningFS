8286632ff5bfade9a8c5ffc64df1b01ef7968e15
oldoc63 <leopoldo.olmos@gmail.com>
2021-12-03 08:05:17 -0400

    Map generate a new array (original remains) #129

--- iterators/main.js
+++ iterators/main.js
@@ -1,5 +1,13 @@
-const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 
-// Iterate over fruits below
-fruits.forEach(fruit => console.log('I want to eat a ' + fruit));
+// Create the secretMessage array below
+const secretMessage = animals.map(firstCharacter => {return firstCharacter[0];})
 
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(smallNum => smallNum/100)
+
+console.log(smallNumbers);
