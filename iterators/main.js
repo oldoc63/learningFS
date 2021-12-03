4a38a18d11dc7c999e5fdf3055c577f97de2bf46
oldoc63 <leopoldo.olmos@gmail.com>
2021-12-03 19:35:10 -0400

    Using findIndex #133

--- iterators/main.js
+++ iterators/main.js
@@ -1,21 +1,7 @@
-const jumbledNums =  [123, 25, 78, 5, 9];
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
 
-const lessThanTen = jumbledNums.findIndex(num => {
-	return num < 10;
-});
+const foundAnimal = animals.findIndex(animal => {if (animal === 'elephant') {return true;}})
 
-/* (1)jumbledNums is an array that contains elements that are numbers. 
- * (2)const lessThanTen = declares a new variable that stores the returned index number from invoking .findIndex().
- * (3)The callback function is an arrow function that has a single parameter, num. Each element in the jumbledNums array will be passed to this function as an argument.
- * (4)num < 10; is the condition that elements are checked against. .findIndex() will return the index of the first element which evaluates to true for that condition.
- */
-console.log(lessThanTen);
-console.log(jumbledNums[3]);
+const startsWithS = animals.findIndex(animal => {if (animal[0] === 's') {return true;}})
 
-/* (5)If there isn’t a single element in the array that satisfies the condition in the callback, then .findIndex() will return -1. 
- */
-const greaterThan1000 = jumbledNums.findIndex(num => {
-  return num > 1000;
-});
- 
-console.log(greaterThan1000);
+
