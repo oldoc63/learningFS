53b9e4af85fe787ac63f28af01397ce009ee8feb
oldoc63 <leopoldo.olmos@gmail.com>
2021-12-04 08:34:47 -0400

    The first argument is the callback function #136

--- iterators/main.js
+++ iterators/main.js
@@ -1,7 +1,23 @@
-const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+// (1) numbers is an array that contains numbers
+const numbers = [1, 2, 4, 10];
 
-const foundAnimal = animals.findIndex(animal => {if (animal === 'elephant') {return true;}})
+// (2) summedNums is a variable that stores the returned value of invoking .reduce()
 
-const startsWithS = animals.findIndex(animal => {if (animal[0] === 's') {return true;}})
+// (3) numbers.reduce calls the .reduce() method on the numbers array and takes in a callback function as an argument.
 
+const summedNums = numbers.reduce((accumulator, currentValue) => {
+	return accumulator + currentValue
+})
 
+// (4) The callback function has two parameters, accumulator and currentValue. The value of accumulator starts off as the value of the first element in the array and the current value starts as the second element.
+
+console.log(summedNums);
+
+// The .reduce method can also take an optional second parameter to set an initial value for accumulator (remember, the first argument is the callback function).
+
+const numbers2 = [1, 2, 4, 10];
+
+const summedNums2 = numbers2.reduce((accumulator, currentValue) => {
+	return accumulator + currentValue}, 100) // <- Second argument for .reduce
+
+console.log(summedNums2);
