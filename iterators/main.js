5088a4a2cec1fe60ac8c52163dad3cd774d9a608
oldoc63 <leopoldo.olmos@gmail.com>
2021-12-04 08:58:41 -0400

    Second argument of .reduce() comes after the callback function #136

--- iterators/main.js
+++ iterators/main.js
@@ -1,23 +1,9 @@
-// (1) numbers is an array that contains numbers
-const numbers = [1, 2, 4, 10];
+const newNumbers = [1, 3, 5, 7];
 
-// (2) summedNums is a variable that stores the returned value of invoking .reduce()
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue;
+}, 10);
 
-// (3) numbers.reduce calls the .reduce() method on the numbers array and takes in a callback function as an argument.
-
-const summedNums = numbers.reduce((accumulator, currentValue) => {
-	return accumulator + currentValue
-})
-
-// (4) The callback function has two parameters, accumulator and currentValue. The value of accumulator starts off as the value of the first element in the array and the current value starts as the second element.
-
-console.log(summedNums);
-
-// The .reduce method can also take an optional second parameter to set an initial value for accumulator (remember, the first argument is the callback function).
-
-const numbers2 = [1, 2, 4, 10];
-
-const summedNums2 = numbers2.reduce((accumulator, currentValue) => {
-	return accumulator + currentValue}, 100) // <- Second argument for .reduce
-
-console.log(summedNums2);
+console.log(newSum); // with second argument
