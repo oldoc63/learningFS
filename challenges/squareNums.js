1e1be4018efb3ffe200c2ffcd65f9a1ed11f0468
oldoc63 <leopoldo.olmos@gmail.com>
2021-12-08 07:45:17 -0400

    .map a fuction #154

---
+++ challenges/squareNums.js
@@ -0,0 +1,10 @@
+//Write a function, squareNums(), that takes in an array of numbers and, using .map(), returns an array with the square of each of the elements of that array. 
+const numbers = [2, 7, 9, 171, 52, 33, 14]
+
+const toSquare = num => num * num
+
+// Write your code here:
+
+const squareNums = arr => arr.map(toSquare)
+
+squareNums(numbers);
