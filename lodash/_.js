55198e6932cdb5cd1b9b5d19881c86a15dba826c
oldoc63 <leopoldo.olmos@gmail.com>
2021-12-15 19:51:33 -0400

    Creates an array of elements split into groups the length of size #199

--- lodash/_.js
+++ lodash/_.js
@@ -80,6 +80,16 @@ dropWhile(array, predicate){
 	return droppedArray;
  },
 
+chunk(array, size=1){
+	let arrayChunks = [];
+	for(let i = 0; i < array.length; i += size){
+		let arrayChunk = array.slice(i, i + size);
+		arrayChunks.push(arrayChunk);
+	}
+	return arrayChunks
+ },
+
+
 };
 
 
