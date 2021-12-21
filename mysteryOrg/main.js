a1c6134a62a1e17f1c3d62f620cf578f974be396
oldoc63 <leopoldo.olmos@gmail.com>
2021-12-21 14:28:41 -0400

    Simulate P. aequor high rate of mutation #224

--- mysteryOrg/main.js
+++ mysteryOrg/main.js
@@ -16,8 +16,23 @@ const mockUpStrand = () => {
 const pAquourFactory = (num, arr) => {
 	return {
 		specimenNum: num,
-		dna: arr
+		dna: arr,
+        mutate() {
+			console.log(`Mutating specimen${this.specimenNum}, with bases: ${this.dna}`); // info message
+            let randomIndex = Math.floor(Math.random() * this.dna.length);                // random index of the base to change
+            console.log("Base to change at index: " + randomIndex);                       // log message of the index to change
+            let randomBase = returnRandBase();                                            // random base to replace
+            console.log("New random base to insert: " + randomBase);                      // logs the random base to use as replacer
+            while (randomBase === this.dna[randomIndex]) {                                      // loop until new random base is diff from the old base
+				console.log("The two bases are equal, will generate a new random base");  // logs if random and old base are equal
+				randomBase = returnRandBase();                                            // generating a random base
+			}
+			this.dna.splice(randomIndex, 1, randomBase);                                 // replacing the base with a random one
+			console.log(`New dna strand: ${this.dna}`)
+		},
 	}
 };
 
-console.log(pAquourFactory(1, mockUpStrand()));
+sample1 = pAquourFactory(1, mockUpStrand());
+sample1.mutate()
+
