f16040e8f284aaaeeed851d19a2d7f229cdbe5a5
oldoc63 <leopoldo.olmos@gmail.com>
2022-05-21 16:18:59 -0400

    When we call extends in a class declaration, all of the parent methods are available to the child class #929

--- jsClasses/main.js
+++ jsClasses/main.js
@@ -26,3 +26,7 @@ class Nurse extends HospitalEmployee {
 
 const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics'])
 
+nurseOlynyk.takeVacationDays(5);
+
+console.log(nurseOlynyk.remainingVacationDays);
+
