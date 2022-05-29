9c945b0f37f22671f50c9d580d34cae161c35cc8
oldoc63 <leopoldo.olmos@gmail.com>
2022-05-29 13:05:12 -0400

    Getters for School #973

--- schoolCatalogue/app.js
+++ schoolCatalogue/app.js
@@ -4,4 +4,13 @@ class School {
         this._level = level;
         this._numberOfStudents = numberOfStudents;
     }
+    get name(){
+        return this._name;
+    }
+    get level(){
+        return this._level;
+    }
+    get numberOfStudents(){
+        return this._numberOfStudents;
+    }
 }
