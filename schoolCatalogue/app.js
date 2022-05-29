d72fe3e67138e6b910807fd1c0ae35d5213ab483
oldoc63 <leopoldo.olmos@gmail.com>
2022-05-28 20:58:28 -0400

    Inside the constructor(), set the School properties and prepend them with an underscore #969

--- schoolCatalogue/app.js
+++ schoolCatalogue/app.js
@@ -1,5 +1,7 @@
 class School {
     constructor(name, level, numberOfStudents){
-        
+        this._name = name;
+        this._level = level;
+        this._numberOfStudents = numberOfStudents;
     }
 }
