a9b0140224d33ccbf9ddb64eb515a913609ca582
oldoc63 <leopoldo.olmos@gmail.com>
2022-05-31 19:31:50 -0400

    Update the secret-messages.js with import #997

--- modules/secret-messages/secret-messages.js
+++ modules/secret-messages/secret-messages.js
@@ -1,15 +1,11 @@
 /* secret-messages.js */
+import { toggleHiddenElement, changeToFunkyColor } from "../modules/dom-functions.js";
+
 const buttonElement = document.getElementById('secret-button');
 const pElement = document.getElementById('secret-p');
 
-const toggleHiddenElement = (domElement) => {
-    if (domElement.style.display === 'none') {
-        domElement.style.display = 'block';
-    }else{
-        domElement.style.display = 'none';
-    }
-}
 
 buttonElement.addEventListener('click', () => {
     toggleHiddenElement(pElement);
+    changeToFunkyColor(buttonElement);
 });
