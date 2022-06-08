511b73c67fd4eb147436c10f4baf32be48a4a399
oldoc63 <leopoldo.olmos@gmail.com>
2022-06-08 16:14:14 -0400

    Create movieTime function #1038

--- challengeClasses/classNetwork.js
+++ challengeClasses/classNetwork.js
@@ -3,4 +3,15 @@ class Network {
         this.data = data;
         this.users = users;
     }
+
+    movieTime(){
+        if ((this.users * 5) <= (this.data - 10)){
+            return true;
+        }else{
+            return false;
+        } 
+    }
 }
+
+const library = new Network(20, 2) 
+console.log(library.movieTime())
