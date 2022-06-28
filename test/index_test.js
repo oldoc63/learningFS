40782413c403a90d3f16602e5243aef2244a4a04
oldoc63 <leopoldo.olmos@gmail.com>
2022-06-27 20:11:25 -0400

    We group tests using the describe function and define tests using the it function #1048

---
+++ test/index_test.js
@@ -0,0 +1,10 @@
+describe('Math', () => {
+    describe('.max', () => {
+      it('returns the argument with the highest value', () => {
+        // Your test goes here
+      });
+      it('returns -Infinity when no arguments are provided', () => {
+        // Your test goes here
+      });
+    });
+  });
